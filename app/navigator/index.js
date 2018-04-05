bf87b1d029ff0ec021918b9fbf50068de4a5b829
Inti <ipiolanti@gmail.com>
2018-04-05 19:42:12 -0300

    Added react navigation

---
+++ app/navigator/index.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { StackNavigator } from 'react-navigation';
+import HomeScene from '../scenes/home';
+import DemoScene from '../scenes/demo';
+
+/**
+ * Main Stack Navigation. Used to display the app header
+ */
+export default StackNavigator({
+  home: {
+    screen: HomeScene
+  },
+  demo: {
+    screen: DemoScene
+  }
+}, {
+  initialRouteName: 'home'
+});
+
+
